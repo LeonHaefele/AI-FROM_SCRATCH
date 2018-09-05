c8becfb9e516d76644942b632204b8118c8b559e
Leon Häfele <haefele.leon@gmail.com>
2018-09-05 16:53:22 +0200

    Update AI.py
    save Neural Network andreload neural network Functionality

--- AI.py
+++ AI.py
@@ -1,5 +1,6 @@
 from random import random
 from math import exp
+import json
 
 #this function is used to create arrays with random floats between -0.5 and 0.5
 #x = the length of the array
@@ -47,9 +48,9 @@ class Layer:
         for a in range(L_Length_int):
             n = Neuron(J_Length_int)
             self.Neurons.append(n)
-#the method calculate output calculates the output of this whole layer. 
-#it takes the output of the Layer before (or the user given Input incase of an input Layer)
-#and returns an array with one item for each Neuron output this array can be given into the next Layer as Input
+    #the method calculate output calculates the output of this whole layer. 
+    #it takes the output of the Layer before (or the user given Input incase of an input Layer)
+    #and returns an array with one item for each Neuron output this array can be given into the next Layer as Input
     def calculate_output(self, input_array):
         self.output = []
         for a in range(len(self.Neurons)):
@@ -68,17 +69,20 @@ class Layer:
 #read its output
 
 #In Fututure more Functionality will be coming
-class CNN:
-    def __init__(self, L_Length_array, Input_Length_int, training_rate=0.05):
-        self.Layers = []
-        self.Input_Length_int = Input_Length_int
-        self.training_rate = training_rate
-        for n in range(len(L_Length_array)):
-            if n != 0:
-                x = Layer(L_Length_array[n], L_Length_array[n-1])
-            else:
-                x = Layer(L_Length_array[n], 1)
-            self.Layers.append(x)
+class NN:
+    def __init__(self, L_Length_array=[10, 10, 5, 2], Input_Length_int=10, training_rate=0.05, reload=False):
+        if not reload:
+            self.Layers = []
+            self.Input_Length_int = Input_Length_int
+            self.training_rate = training_rate
+            for n in range(len(L_Length_array)):
+                if n != 0:
+                    x = Layer(L_Length_array[n], L_Length_array[n-1])
+                else:
+                    x = Layer(L_Length_array[n], 1)
+                self.Layers.append(x)
+
+
     #this function feeds the Input data into the CNN and returns its output
     #this function doesnt manipulate the Network
     def feed_forwoard(self, input_array):
@@ -88,6 +92,8 @@ class CNN:
             for Layer in self.Layers:
                 input_array = Layer.calculate_output(input_array)
             return input_array
+
+
     #this function includes the whole Traing for one Training step
     #you should use this in a "for" or "while" structure
     def train(self, input_array, expected_array):
@@ -113,7 +119,7 @@ class CNN:
                     #better read abaout it at this place:
                     #https://de.wikipedia.org/wiki/Backpropagation#Fehlerminimierung
                     #its where I got the equations from
-#---------------------------------------------------------------------------------
+                    #---------------------------------------------------------------------------------
                     error_1 = sigmoid_I(reversed_layer[L].output[N])
      
 
@@ -132,4 +138,22 @@ class CNN:
                     error_signal_new.append(error)
                     delta_w = self.training_rate*error*reversed_layer[L].output[N]
                     reversed_layer[L].Neurons[N].weights[W] += delta_w
-#-------------------------------------------------------------------------------------------------------
+                    #---------------------------------------------------------------------------------
+
+    def save_NN(self, NN_file):
+        f = open(NN_file, "wb")
+        the_three_important = [self.Layers, self.training_rate, self.Input_Length_int]
+
+        NN_as_string = json.dumps(the_three_important)
+        f.write(NN_as_string)
+        f.close()
+
+    def reload_NN(self, NN_file):
+        f = open(NN_file, "rb")
+        NN_as_string = f.read()
+        data = json.loads(NN_as_string)
+        self.Input_Length_int = data[2]
+        self.training_rate = data[1]
+        self.Layers = data[0]
+
+
